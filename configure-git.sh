a5b38b0b5a8cd45143277bf3449182dec92d04a7
Evan W. Noronha <hello@evannoronha.com>
2018-01-09 23:19:12 -0800

    Add github config to git config script

--- configure-git.sh
+++ configure-git.sh
@@ -1,12 +1,21 @@
-echo '###Congigure Git..'
+echo '### Configure Git..'
 
-echo "Enter the Global Username for Git:";
+echo "Enter your first and last name for Git:";
 read GITUSER;
 git config --global user.name "${GITUSER}"
 
-echo "Enter the Global Email for Git:";
+echo "Enter your email address for Git:";
 read GITEMAIL;
 git config --global user.email "${GITEMAIL}"
+
+echo "Enter your Github username:";
+read GITHUBUSER;
+git config --global github.user "${GITHUBUSER}"
+
+echo "Go here and generate a token: https://github.com/settings/tokens/new";
+echo "Enter your Github personal access token:";
+read GITHUBTOKEN;
+git config --global user.email "${GITHUBTOKEN}"
 
 echo 'Git has been configured!'
 git config --list
